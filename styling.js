63374936a775af1335b0f7d51b35b94d4170d464
Sandesh Banakar I <isandeshbanakar@gmail.com>
2022-09-30 23:48:17 +0530

    added new line of JS

--- styling.js
+++ styling.js
@@ -1,4 +1,13 @@
 function modify() {
-    document.getElementById("main");
-    alert("Get your mouse off!")
+    document.getElementById("main").style.fontSize="100px";
+
+    document.getElementById("main").style.color="red";
+
+    document.getElementById("main").style.textAlign="center";
+ 
+    // alert("Get your mouse off!")
+}
+
+function unmodify() {
+
 }
